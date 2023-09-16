7b3b98e7f4f9444f9028afcb7d3602d24c342e66
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-09-16 11:14:06 +0300

    used let to create variabes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let currentOperator ='';
+let preValue = O;
 
+
+function appendToDisplay(value) {
+    currentInput +=value;
+    display.innerText = currentInput;
+}
