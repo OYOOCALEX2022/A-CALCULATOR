ac31efdca2a4c29709a296a17908717e662056cb
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-09-16 12:19:20 +0300

    added function calculate result

--- scripts.js
+++ scripts.js
@@ -15,3 +15,37 @@ currentOperator = '';
 preValue = 0;
 display.innerText = '0';
 }
+
+function calculateResult() {
+    if (currentInput === '') return;
+    const currentValue = parseFoat(currentInput);
+
+    switch (currentOperator) {
+        case '+':
+             preValue +=currentValue;
+             break;
+        case '-':
+             preValue -=currentValue;
+             break;
+        case '*':
+            preValue *=currentValue;
+             break;
+        case '/':
+             if (currentValue !== 0) {
+                 prevalue /= currentValue;
+             } else {
+                alert ('Error: Division by zero');
+                clearDisplay();
+                return;
+             }
+             break;
+         default:
+               preValue = currentValue;
+               break;
+       }
+    display.innerText = preValue;
+    currentInput ='';
+    currentOperator='';
+}  
+
+clearDisplay();
